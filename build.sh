7be482d617d48aad937721bb9294e08457668f24
huyentdh <lop9a2vip@gmail.com>
2024-09-15 23:05:33 +0700

    Beaglebone black yocto.
    Signed-off-by: huyentdh <lop9a2vip@gmail.com>

---
+++ build.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+# Script to build image for qemu.
+# Author: Siddhant Jajoo.
+
+git submodule init
+git submodule sync
+git submodule update
+
+# local.conf won't exist until this step on first execution
+source poky/oe-init-build-env
+
+CONFLINE="MACHINE = \"beaglebone-yocto\""
+
+cat conf/local.conf | grep "${CONFLINE}" > /dev/null
+local_conf_info=$?
+
+if [ $local_conf_info -ne 0 ];then
+	echo "Append ${CONFLINE} in the local.conf file"
+	echo ${CONFLINE} >> conf/local.conf
+	
+else
+	echo "${CONFLINE} already exists in the local.conf file"
+fi
+
+CONFLINE="IMAGE_INSTALL:append = \" vim\""
+
+cat conf/local.conf | grep "${CONFLINE}" > /dev/null
+local_conf_info=$?
+
+if [ $local_conf_info -ne 0 ];then
+	echo "Append ${CONFLINE} in the local.conf file"
+	echo ${CONFLINE} >> conf/local.conf
+	
+else
+	echo "${CONFLINE} already exists in the local.conf file"
+fi
+
+CONFLINE="IMAGE_INSTALL:append = \" dropbear\""
+
+cat conf/local.conf | grep "${CONFLINE}" > /dev/null
+local_conf_info=$?
+
+if [ $local_conf_info -ne 0 ];then
+	echo "Append ${CONFLINE} in the local.conf file"
+	echo ${CONFLINE} >> conf/local.conf
+	
+else
+	echo "${CONFLINE} already exists in the local.conf file"
+fi
+
+bitbake-layers add-layer ../meta-meta-openembedded/meta*
+bitbake-layers add-layer ../meta-beagleboard/meta-beagleboard-extras
+
+set -e
+bitbake core-image-minimal
